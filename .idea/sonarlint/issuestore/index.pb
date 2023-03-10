a5b460503b248ad4634487dcd7b4d7a4b465a18d
Игорь Орлов <test@testsymfony.test>
2023-03-10 08:12:56 +0700

    dz_2_tests

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+<
+build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
+M
+src/main/resources/result.csv,e/8/e8ccf7cd778840db1c3318fb075c0f9c8b462a6c
--- src/test/java/homework/model/ResultTest.java
+++ src/test/java/homework/model/ResultTest.java
@@ -2,27 +2,45 @@ package homework.model;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import java.util.Objects;
 class ResultTest {
+
+    String MGender = "M";
+    String FGender = "F";
+    
+    String five_km = "5 км";
+    String ten_km = "10 км";
+
+    String name = "name";
 
     @Test
     void hasGender() {
+        if (Objects.equals(this.MGender, "M")){
+            System.out.println(MGender);
+        } else if (Objects.equals(this.FGender, "F")) {
+            System.out.println(FGender);
+        } else {
+            System.out.println("Error! No gender");
+        }
     }
 
     @Test
     void hasDistance() {
+        if (Objects.equals(this.five_km, "5 км")) {
+            System.out.println(five_km);
+        } else if (Objects.equals(this.ten_km, "10 км")) {
+            System.out.println(ten_km);
+        } else {
+            System.out.println("Error! No distance");
+        }
     }
 
     @Test
     void person() {
-    }
-
-    @Test
-    void distance() {
-    }
-
-    @Test
-    void time() {
+        if (Objects.equals(this.name, "name")){
+            System.out.println(name);
+        } else {
+            System.out.println("Error! No name");
+        }
     }
 }
